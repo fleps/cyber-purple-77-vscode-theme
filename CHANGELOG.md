4d6ec4ab81abbd542cb92f94eaacd51b9c3b6e29
Felipe Spengler <20216453+fleps@users.noreply.github.com>
2024-07-03 23:34:41 -0300

    Changes for new release v1.1.0
    - `gitDecoration` file name foreground colors adjusted to follow the theme style better (apply to the filename on the sidebar and the tabs).
      - Most notable one is MODIFIED, from the default washed yellow/beige to a slightly purple color.
    - Improved JAVA support
    - Added Salesforce ISML proper colors
    - Readme fixes and updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,14 @@
 
 # Change Log
 
+## v1.1.0 - Jun 3, 2024
 
-## [1.0.0]
+- `gitDecoration` file name foreground colors adjusted to follow the theme style better (apply to the filename on the sidebar and the tabs).
+  - Most notable one is MODIFIED, from the default washed yellow/beige to a slightly purple color.
+- Improved JAVA support
+- Added Salesforce ISML proper colors
+- Readme fixes and updated
+
+## v1.0.0 - Jun 2, 2024
 
 - Initial release
--- code-samples/cyberpurple.java
+++ code-samples/cyberpurple.java
@@ -28,3 +28,46 @@ public class cyberpunk {
       2090,
       new String[] { "Cops", "Government" });
 }
+
+
+@Override
+public void run() {
+    final double TARGET_FRAMES_PER_SECOND = 60.0;
+    final double TARGET_UPDATES_PER_SECOND = 30.0;
+    final double NANOSECONDS_PER_SECOND = 1000000000.0;
+
+    double targetNanosecondsPerFrame = NANOSECONDS_PER_SECOND / TARGET_FRAMES_PER_SECOND;
+    double targetNanosecondsPerUpdate = NANOSECONDS_PER_SECOND / TARGET_UPDATES_PER_SECOND;
+
+    long lastUpdateTime = System.nanoTime();
+    long lastFrameTime = lastUpdateTime;
+    int frames = 0;
+    int updates = 0;
+
+    while (true) {
+        long currentTime = System.nanoTime();
+        double deltaTimeUpdate = (currentTime - lastUpdateTime) / targetNanosecondsPerUpdate;
+        double deltaTimeFrame = (currentTime - lastFrameTime) / targetNanosecondsPerFrame;
+
+        lastUpdateTime = currentTime;
+
+        while (deltaTimeUpdate >= 1) {
+            update();
+            updates++;
+            deltaTimeUpdate--;
+        }
+
+        if (deltaTimeFrame >= 1) {
+            gamePane.repaint();
+            frames++;
+            deltaTimeFrame--;
+        }
+
+        if (System.currentTimeMillis() - lastUpdateTime >= 1000) {
+            System.out.println("FPS: " + frames + ", UPS: " + updates);
+            frames = 0;
+            updates = 0;
+            lastUpdateTime += 1000;
+        }
+    }
+}
